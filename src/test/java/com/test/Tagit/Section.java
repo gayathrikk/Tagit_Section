86b9dcf07c2a6f46788ca1fd0c65cbbb3feeae86
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2024-06-21 11:04:40 +0530

    Section.java

--- src/test/java/com/test/Tagit/Section.java
+++ src/test/java/com/test/Tagit/Section.java
@@ -32,7 +32,7 @@ public class Section {
 			public void setup() throws Exception
 			{
 			DesiredCapabilities dc = DesiredCapabilities.chrome();
-			       URL url = new URL("http://172.12.20.99:4444/wd/hub");
+			       URL url = new URL("http://172.20.23.92:4444/wd/hub");
 			       driver = new RemoteWebDriver(url, dc);
 			       wait = new WebDriverWait(driver, 30);
 		
